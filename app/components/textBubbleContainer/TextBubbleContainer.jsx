e8b98fefc78487ae5bf8a55ac49e16ed4fc0e1c6
Franco Ortega <francopdx@gmail.com>
2024-12-02 18:41:33 -0800

    create TextBubbleTrail to display trail of bubbles leading up to TextBubble

--- app/components/textBubbleContainer/TextBubbleContainer.jsx
+++ app/components/textBubbleContainer/TextBubbleContainer.jsx
@@ -1,5 +1,11 @@
+import TextBubbleTrail from '../textBubbleTrail/TextBubbleTrail';
 import styles from './TextBubbleContainer.module.css';
 
 export default function TextBubbleContainer({ children }) {
-	return <div className={styles.TextBubbleContainer}>{children}</div>;
+	return (
+		<div className={styles.TextBubbleContainer}>
+			{children}
+			<TextBubbleTrail />
+		</div>
+	);
 }
---
+++ app/components/textBubbleTrail/TextBubbleTrail.jsx
@@ -0,0 +1,11 @@
+import styles from './TextBubbleTrail.module.css';
+
+export default function TextBubbleTrail() {
+	return (
+		<div className={styles.TextBubbleTrail}>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	);
+}
